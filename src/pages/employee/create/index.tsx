1fd0f7f05191b60e2eb9cae7157a3f1c22d1e660
RusdyZain <ryanfajri28@gmail.com>
2024-03-12 17:33:21 +0800

    Progress Create

---
+++ src/pages/employee/create/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CreateEmployeePage() {
+  return (
+    <div className="max-w-md mx-auto mt-5">
+      <h1 className="text-2xl text-center mb-2">Add New Employee</h1>
+      <div>Form</div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import Employee from "./employee";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <Employee />;
 }
